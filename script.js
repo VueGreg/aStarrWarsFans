ef2d6e9b342186375a1b2476509a886f5b732f68
VueGreg <116952592+VueGreg@users.noreply.github.com>
2023-06-21 14:36:46 +0200

    add style effect

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ window.addEventListener('scroll' ,() => {
 
     const {scrollTop, clientHeight} = document.documentElement
 
+    // Explication de la ligne de code
+
+    //const myScrollTop = document.documentElement.scrollTop
+    //const myClientHeight = document.documentElement.clientHeight
+
     const topElementToTopViewport = second.getBoundingClientRect().top
 
     if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5) {
