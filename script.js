76bc83c5dc704a4de84ad56fc815d19c2e370242
VueGreg <116952592+VueGreg@users.noreply.github.com>
2023-06-21 19:03:23 +0200

    add: data with API for charge nav

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ window.addEventListener('scroll' ,() => {
         second.classList.add('active')
     }
 })
+
+const list = document.querySelector('ul')
+
+list.addEventListener('click', (e) => {
+    let itemMenu = e.target.getAttribute('value')
+    apiCall(itemMenu, e.target.tagName, e.target)
+})
+
+const apiCall = (word, nameTarget, typeTarget) => {
+    const second = document.querySelector('.second')
+    second.classList.add('inactive')
+    let loadCard = document.querySelector('.loading-card')
+    loadCard.style.visibility = 'visible'
+    if (typeof word === "string" ) {
+        fetch(`https://swapi.dev/api/${word}/?format=json`)
+        .then (response => response.json())
+        .then ((data) => {
+            if (word !== 'films') {
+                const tab = Object.values(data)
+                let newUl = document.createElement('ul')
+                newUl.classList.add('list-over')
+
+                const nav = document.querySelector('nav')
+                nav.classList.add('upperHeigth')
+
+                if (nameTarget === "LI") {
+                    typeTarget.appendChild(newUl)
+                    loadCard.style.visibility = 'hidden'
+                }
+
+                tab[3].forEach(element => {
+                    const newLi = document.createElement('li')
+                    newLi.textContent = element.name
+                    newUl.appendChild(newLi)
+                    
+                });
+            }
+            
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+}
